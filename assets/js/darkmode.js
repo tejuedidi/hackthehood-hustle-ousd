dc77062aa9cff02046746176654d65d06ad5fc48
Tejaswini Edidi <tejuedidi@gmail.com>
2025-06-03 22:42:42 -0700

    added functional dark mode theme toggle
    Signed-off-by: Tejaswini Edidi <tejuedidi@gmail.com>

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,3 +1,29 @@
-document.getElementById('dark-mode-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
+function toggleTheme() {
+    const currentTheme = localStorage.getItem("preferred-theme") || "light";
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+
+    if (window.jtd) { // just-the-docs theme switcher
+      jtd.setTheme(newTheme);
+    }
+
+    // save preference and update button
+    localStorage.setItem("preferred-theme", newTheme);
+    updateToggleButton(newTheme);
+}
+
+function updateToggleButton(theme) {
+    const btn = document.getElementById("mode-toggle");
+    if (btn) {
+      btn.innerHTML = theme === "dark" ? "⏾" : "☼";
+    }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    const savedTheme = localStorage.getItem("preferred-theme") || "light";
+
+    if (window.jtd) {
+      jtd.setTheme(savedTheme);
+    }
+
+    updateToggleButton(savedTheme);
 });
